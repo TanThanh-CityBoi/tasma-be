48403eb82c87de11dd4d56071e975272b9295787
tanthanh-cityboi <tanthanhe@gmail.com>
2023-03-02 18:22:06 +0700

    eslint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { LoginResponseDto } from './dto/response';
-import { LocalStrategy } from './local.stragy';
+
 @Injectable()
 export class AuthService {
   async login(data: any): Promise<any> {
-    const { email, password } = data;
+    const { email } = data;
     const user = { email, firstname: 'first name', lastName: 'lastname' };
     return plainToClass(LoginResponseDto, user, {
       excludeExtraneousValues: true,
