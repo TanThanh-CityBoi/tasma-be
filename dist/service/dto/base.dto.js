a5a771ee63876f51132a41f5b3ebe39b358fcc20
thanh.nguyen <thanh.nguyen@codestringers.com>
2023-05-13 00:47:14 +0700

    ignore dist

--- dist/service/dto/base.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * A DTO base object.
- */
-class BaseDTO {
-}
-exports.BaseDTO = BaseDTO;
-//# sourceMappingURL=base.dto.js.map
